891dbd2c984092fe7dc59df0c34184679b80f037
Ilya Maximets <i.maximets@samsung.com>
2019-05-28 17:06:50 +0300

    Drop bmi2intrin.h.
    Change-Id: Ia854b4ace6c28c5d94577a71837232f920bd82de
    Signed-off-by: Ilya Maximets <i.maximets@samsung.com>

--- include/sparse/automake.mk
+++ include/sparse/automake.mk
@@ -11,7 +11,6 @@ noinst_HEADERS += \
         include/sparse/arpa/inet.h \
         include/sparse/bits/floatn.h \
         include/sparse/assert.h \
-        include/sparse/bmi2intrin.h \
         include/sparse/math.h \
         include/sparse/netinet/in.h \
         include/sparse/netinet/ip6.h \
