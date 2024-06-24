c200e5733e6c6d088df0814a5f258f5b606dfb5a
Ilya Maximets <i.maximets@ovn.org>
2024-06-25 00:36:02 +0200

    raft: Don't fail precheck on non-consequential commands.
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- ovsdb/raft.c
+++ ovsdb/raft.c
@@ -2320,7 +2320,7 @@ raft_precheck_prereq(const struct raft *raft, const struct uuid *prereq)
 {
     if (!uuid_equals(raft_current_eid(raft), prereq)) {
         VLOG_INFO(
-            "Prerequisites do not match: expected "UUID_FMT", got "UUID_FMT,
+            "prerequisites do not match: expected "UUID_FMT", got "UUID_FMT,
             UUID_ARGS(prereq), UUID_ARGS(raft_current_eid(raft)));
         return false;
     }
@@ -2336,13 +2336,24 @@ raft_precheck_prereq(const struct raft *raft, const struct uuid *prereq)
      * Incomplete commands on a leader will not change the leader's current
      * 'eid' on commit as they are already part of the leader's log. */
     if (raft->role != RAFT_LEADER && hmap_count(&raft->commands)) {
-        VLOG_INFO("Follower still has incomplete commands on prereq "UUID_FMT,
-                  UUID_ARGS(prereq));
-        log_all_commands(raft);
-        return false;
+        struct raft_command *cmd;
+
+        HMAP_FOR_EACH (cmd, hmap_node, &raft->commands) {
+            /* It is possible that we still have incomplete commands from
+             * the time this server was a leader, skip those.  Also skip
+             * commands that do not carry any data, as they can't change
+             * prerequisites.  This isn't just an optimization, because
+             * the database will not re-run triggers unless data changes
+             * or one of the data-carrying triggers completes. */
+            if (!cmd->index && !uuid_is_zero(&cmd->eid)) {
+                VLOG_INFO("follower still an incomplete command "UUID_FMT,
+                          UUID_ARGS(&cmd->eid));
+                return false;
+            }
+        }
     }
 
-    VLOG_INFO("Prerequisites "UUID_FMT" success!", UUID_ARGS(prereq));
+    VLOG_INFO("prerequisites "UUID_FMT" match!", UUID_ARGS(prereq));
     return true;
 }
 
